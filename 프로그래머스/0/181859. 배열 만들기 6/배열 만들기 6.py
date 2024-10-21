a82364bba539f61d5d917c9473b97ec2961bd0c0
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-10-21 10:27:36 +0900

    [level 0] Title: 배열 만들기 6, Time: 169.88 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if len(answer)==0:
+            answer.append(arr[i])
+        elif answer[-1]==arr[i]:
+            answer.pop(-1)
+        else:
+            answer.append(arr[i])
+    if len(answer)==0:
+        answer.append(-1)
+    return answer
