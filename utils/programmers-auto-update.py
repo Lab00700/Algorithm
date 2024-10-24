1f43566e75114f68c08aaaaa48b473d56c05ce71
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-10-24 13:06:32 +0900

    Update programmers-auto-update.py

--- utils/programmers-auto-update.py
+++ utils/programmers-auto-update.py
@@ -9,17 +9,15 @@ def count_programmers_info():
     for level in levels:
         problems_list = os.listdir(f"./프로그래머스/{level}")
         count = len(problems_list)
-        count_info+=f"- {level} 레벨 문제 수 : {count}\n"
+        count_info+=f"- {level} 레벨 : {count}\n"
         total_count += count
 
-    programmers_info = ("# 프로그래머스\n"
-            f"### 프로그래머스 레벨 별 문제 수\n"
-            f"총 문제 수 : {total_count}\n\n"
-            f"마지막 업데이트 : {datetime.now(timezone(timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}\n\n")
+    programmers_info = (f"### 프로그래머스 - {datetime.now(timezone(timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}\n"
+                        f"#### 총 문제 수 : {total_count}\n")
     programmers_info+=count_info
     return programmers_info
 
 if __name__ == "__main__":
     solving_count = count_programmers_info()
-    with open("./md_files/Solving-count.md", 'w', encoding='utf-8') as f:
+    with open("./md_files/programmers-solving-count.md", 'w', encoding='utf-8') as f:
         f.write(solving_count)
