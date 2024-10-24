9fbce08da28562c407b619a52f2eb0349e9abd54
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-10-24 13:48:14 +0900

    [level 1] Title: 소수 찾기, Time: 1783.51 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    temp=[]
+    for i in range(n):
+        temp.append(0)
+    for i in range(n):
+        for k in range(i,n,i+1):
+            temp[k]+=1
+    for i in range(n):
+        if temp[i]==2:
+            answer+=1
+    return answer
