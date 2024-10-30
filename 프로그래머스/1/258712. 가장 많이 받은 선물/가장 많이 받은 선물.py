8aa95b26a0f5bd46728bf4e9df02bbb2e5bb3330
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-10-30 11:13:20 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 10.53 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,41 @@
+def solution(friends, gifts):
+    fr={}
+    gi={}
+    pr={}
+    answer = 0
+    leng=len(friends)
+    for f in friends:
+        fr[f]=0
+        pr[f]=0
+        for r in friends:
+            if f!=r and ' '.join([f,r]) not in gi and ' '.join([r,f]) not in gi:
+                gi[' '.join([f,r])]=0
+                
+    for g in gifts:
+        a,b=g.split()
+        fr[a]+=1
+        fr[b]-=1
+        if g not in gi:
+            gi[' '.join([b,a])]-=1
+        else:
+            gi[g]+=1
+    for g in gi.keys():
+        a,b=g.split()
+        if fr[a]==fr[b]:
+            continue
+        if 0<gi[g]:
+            pr[a]+=1
+        elif 0>gi[g]:
+            pr[b]+=1
+        else:
+            if fr[a]>fr[b]:
+                pr[a]+=1
+            else:
+                pr[b]+=1
+    for p in pr.keys():
+        if answer<pr[p]:
+            answer=pr[p]
+    print(gi)
+    print(fr)
+    print(pr)
+    return answer
