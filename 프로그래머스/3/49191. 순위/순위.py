9ecc152afcdd8192151de3ebd43db2e7a5eefafa
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-11-29 13:11:23 +0900

    [level 3] Title: 순위, Time: 10.61 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,49 @@
+def solution(n, results):
+    dic={}
+    dic_count={}
+    rank=[]
+    answer=0
+    for r in results:
+        if r[0] in dic:
+            dic[r[0]]['win'].add(r[1])
+        else:
+            dic[r[0]] = {'win': {r[1]},
+                         'defeat': set(),
+                         'check':False}
+        if r[1] in dic:
+            dic[r[1]]['defeat'].add(r[0])
+        else:
+            dic[r[1]] = {'win': set(),
+                         'defeat': {r[0]},
+                         'check':False}
+
+    def defeat(current):
+        if not dic[current]['win']:
+            return {current}
+        if dic[current]['check']:
+            return dic[current]['win']
+        temp = dic[current]['win'].copy()
+        for di in dic[current]['win']:
+            for de in defeat(di):
+                temp.add(de)
+        dic[current]['win'] = temp
+        dic[current]['check']=True
+        return dic[current]['win']
+    for d in dic:
+        if not dic[d]['defeat']:
+            temp = dic[d]['win'].copy()
+            for di in dic[d]['win']:
+                for de in defeat(di):
+                    temp.add(de)
+            dic[d]['win']=temp
+            dic[d]['check']=True
+    for d in dic:
+        dic_count[d]=len(dic[d]['win'])
+    for d in dic_count:
+        rank.append(dic_count[d])
+    rank.sort()
+    for i in range(len(rank)):
+        if rank[i]==i:
+            if (i!=len(rank)-1 and rank[i]!=rank[i+1]) or i==len(rank)-1:
+                answer+=1
+    return answer
