1748d03b468400e9fcae137defb79d9b8dcc4262
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-12-03 12:55:28 +0900

    [level 2] Title: N-Queen, Time: 3397.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,12 +1,11 @@
 def solution(n):
-    answer = 0
     def find(chess_y,c):
-        nonlocal answer
         if c==n: #퀸을 모두 사용하였으면 경우의 수 추가
             if n%2 and chess_y[n%2+n//2-1]==0: #n이 홀수인경우 중앙칸 탐색
-                answer+=1
+                return 1
             else: #대칭인 경우
-                answer+=2
+                return 2
+        ans=0
         if c==0: #체스판 중앙을 기준으로 대칭이 성립하므로 첫번째 퀸은 절반만 탐색
             t=n%2+n//2
         else:
@@ -20,6 +19,7 @@ def solution(n):
             if safe: #공격 범위로부터 안전하면 새로운 퀸 배치
                 temp_y=chess_y.copy()
                 temp_y[y]=c
-                find(temp_y,c+1) #재귀
-    find({},0)
+                ans+=find(temp_y,c+1) #재귀
+        return ans
+    answer=find({},0)
     return answer
