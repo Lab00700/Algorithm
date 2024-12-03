56e29e192ed022c5044b6a849c03960396e54790
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-12-03 12:52:07 +0900

    [level 2] Title: N-Queen, Time: 3664.40 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -3,18 +3,18 @@ def solution(n):
     def find(chess_y,c):
         nonlocal answer
         if c==n: #퀸을 모두 사용하였으면 경우의 수 추가
-            if n%2 and chess_y[n%2+n//2-1]==0:
+            if n%2 and chess_y[n%2+n//2-1]==0: #n이 홀수인경우 중앙칸 탐색
                 answer+=1
-            else:
+            else: #대칭인 경우
                 answer+=2
-        if c==0:
+        if c==0: #체스판 중앙을 기준으로 대칭이 성립하므로 첫번째 퀸은 절반만 탐색
             t=n%2+n//2
         else:
             t=n
         for y in range(t): #각 x좌표에는 1개의 퀸만 들어갈 수 있으므로 y만 반복
             safe=True
             for cy in chess_y: #배치된 퀸들의 공격 범위에 현재 좌표가 포함되는지 확인
-                if abs(cy-y)==abs(chess_y[cy]-c) or y==cy:
+                if y==cy or abs(cy-y)==abs(chess_y[cy]-c) :
                     safe=False
                     break
             if safe: #공격 범위로부터 안전하면 새로운 퀸 배치
