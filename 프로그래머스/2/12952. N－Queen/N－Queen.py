34ba85883da199979655b1c20f13ad3c2acd908f
YunJae Park <82380623+Lab00700@users.noreply.github.com>
2024-12-03 12:11:50 +0900

    [level 2] Title: N-Queen, Time: 8898.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -4,15 +4,16 @@ def solution(n):
         nonlocal answer
         if c==n: #퀸을 모두 사용하였으면 경우의 수 추가
             answer+=1
+            return
         for y in range(n): #각 x좌표에는 1개의 퀸만 들어갈 수 있으므로 y만 반복
             safe=True
-            for cy in chess_y: #배치된 퀸들의 공격 범위에 현재 좌표가 포함되는지 확인
-                if abs(cy-y)==abs(chess_y[cy]-c) or y==cy:
+            for k in range(c): #배치된 퀸들의 공격 범위에 현재 좌표가 포함되는지 확인
+                if chess_y[k]==y or abs(k-c)==abs(chess_y[k]-y):
                     safe=False
                     break
             if safe: #공격 범위로부터 안전하면 새로운 퀸 배치
                 temp_y=chess_y.copy()
-                temp_y[y]=c
+                temp_y.append(y)
                 find(temp_y,c+1) #재귀
-    find({},0)
+    find([],0)
     return answer
